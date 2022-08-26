992bf33e5b2271a43ac5f54b2880d65873b80e20
tzigor <igortz@mail.ru>
2022-08-26 11:09:28 +0700

    Updates

---
+++ precache-manifest.7b0880c70f0222af109de97a027478f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "218119c0658afc48fa88",
+    "url": "/static/css/main.414e2556.chunk.css"
+  },
+  {
+    "revision": "218119c0658afc48fa88",
+    "url": "/static/js/main.218119c0.chunk.js"
+  },
+  {
+    "revision": "2cb1527e18f6bd4b39d1",
+    "url": "/static/js/1.2cb1527e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "195bece5ad60469eacdc086d5a52d473",
+    "url": "/index.html"
+  }
+];
