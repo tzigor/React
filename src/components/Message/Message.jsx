81f808a3ac90ca05f513cc57f2b18c0a973c68df
igortz <igortz@mail.ru>
2022-08-26 15:53:40 +0700

    Lesson 1 HW completed

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -6,9 +6,15 @@ function wordCount(str) {
     return m ? m.length : 0;
 }
 
+function plural(n) {
+    if (n === 0) return 'no words'
+    else if (n === 1) return '1 word'
+    else return n + ' words'
+}
+
 export const Message = (props) => {
     return <>
-        <div className={style.header}>New text rceived with {wordCount(props.text)} words:</div>
+        <div className={style.header}>New text rceived with {plural(wordCount(props.text))}:</div>
         <div className={style.message}>{props.text}</div>
     </>
 }
