fe3ce880219c226b41cb1bbf9dc3378aacd5dc60
igortz <igortz@mail.ru>
2022-10-03 16:00:16 +0700

    Lesson 6 started

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const element = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(element);
-root.render(<App />);
+root.render(
+    <BrowserRouter><App /></BrowserRouter>
+);
