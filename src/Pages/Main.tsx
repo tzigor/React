6fd0305e0512b456f5d15e7212a15705867de360
igortz <igortz@mail.ru>
2022-10-04 12:23:32 +0700

    Lesson 6 completed

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -5,40 +5,40 @@ import { ListItem } from '@mui/material';
 import './pages.css';
 
 export const Main: FC<MainProps> = ({ chatList, handleChatList }) => {
-    const [chatName, setChatName] = useState('');
-    const handleSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
-        ev.preventDefault();
-    };
-    const handleClick = () => {
-        if (chatName) {
-            const updateChats: ChatList = [
-                ...chatList,
-                { id: chatList.length + 1, name: chatName, chat: [] },
-            ];
-            handleChatList(updateChats);
-        }
-    };
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <input
-                    className="input"
-                    onChange={(ev) => setChatName(ev.target.value)}
-                />
-                <button className="button" onClick={handleClick}>
-                    Add chat
-                </button>
-            </form>
+  const [chatName, setChatName] = useState('');
+  const handleSubmit = (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+  };
+  const handleClick = () => {
+    if (chatName) {
+      const updateChats: ChatList = [
+        ...chatList,
+        { id: chatList.length + 1, name: chatName, chat: [] },
+      ];
+      handleChatList(updateChats);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          onChange={(ev) => setChatName(ev.target.value)}
+        />
+        <button className="button" onClick={handleClick}>
+          Add chat
+        </button>
+      </form>
 
-            {chatList.map((item, indx) => {
-                return (
-                    <ListItem key={indx}>
-                        <NavLink to={`/chats/${indx + 1}`} >
-                            <p>{item.name}</p>
-                        </NavLink>
-                    </ListItem>
-                );
-            })}
-        </>
-    );
+      {chatList.map((item, indx) => {
+        return (
+          <ListItem key={indx}>
+            <NavLink to={`/chats/${indx + 1}`}>
+              <p>{item.name}</p>
+            </NavLink>
+          </ListItem>
+        );
+      })}
+    </>
+  );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,29 +3,31 @@ import { NavLink, Outlet } from 'react-router-dom';
 import './Header.css';
 
 export const Header: FC = () => {
-    return (
-        <>
-            <header className="menu">
-                <NavLink className="link"
-                    to={'/Main'}
-                    style={({ isActive }) => ({
-                        color: isActive ? 'green' : 'blue',
-                    })}
-                >
-                    Main
-                </NavLink>
-                <NavLink className="link"
-                    to={'/Profile'}
-                    style={({ isActive }) => ({
-                        color: isActive ? 'green' : 'blue',
-                    })}
-                >
-                    Profile
-                </NavLink>
-            </header>
-            <main>
-                <Outlet />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <header className="menu">
+        <NavLink
+          className="link"
+          to={'/Main'}
+          style={({ isActive }) => ({
+            color: isActive ? 'green' : 'blue',
+          })}
+        >
+          Main
+        </NavLink>
+        <NavLink
+          className="link"
+          to={'/Profile'}
+          style={({ isActive }) => ({
+            color: isActive ? 'green' : 'blue',
+          })}
+        >
+          Profile
+        </NavLink>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
 };
